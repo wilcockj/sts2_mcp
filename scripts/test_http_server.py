83ab4170a495ac801b3e54b1869814e5f9a84596
James Wilcock <jameswilcock8@gmail.com>
2026-03-19 19:57:29 -0700

    changed port

--- scripts/test_http_server.py
+++ scripts/test_http_server.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-url = 'http://localhost:15526'
+url = 'http://localhost:15527'
 response = urllib.request.urlopen(url)
 html_content = response.read().decode('utf-8')
 print(html_content)
